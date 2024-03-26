204942546c7953047cadac57cd7f38ebed5e970b
dusklight00 <r.rahul.developer@gmail.com>
2024-03-26 15:52:49 +0530

    login page complete

--- view/src/Login.tsx
+++ view/src/Login.tsx
@@ -1,10 +1,43 @@
-import { IonContent } from "@ionic/react";
+import {
+  IonContent,
+  IonHeader,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+} from "@ionic/react";
 
 const Login: React.FC = () => {
   return (
-    <IonContent className="ion-padding">
-      <h1 className="text-3xl font-bold underline text-red">Hello world!</h1>
-    </IonContent>
+    <div className="h-full">
+      <IonHeader>
+        <IonToolbar>
+          <IonSegment value="all">
+            <IonSegmentButton value="all">
+              <IonLabel>User</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="favorites">
+              <IonLabel>Admin</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="h-full">
+        <div className="p-20 flex flex-col gap-10">
+          <h1 className="text-center text-4xl font-bold">Login</h1>
+          <IonInput label="Username" placeholder="Enter your username" />
+          <IonInput
+            label="Password"
+            type="password"
+            placeholder="Enter your username"
+          />
+          <IonButton className="w-full">Login</IonButton>
+        </div>
+      </IonContent>
+    </div>
   );
 };
 export default Login;
