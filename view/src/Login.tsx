dd6b4923fc5ee6a7754bfd3a50addd0884225e22
dusklight00 <r.rahul.developer@gmail.com>
2024-03-26 15:28:48 +0530

    tailwindcss setup complete

---
+++ view/src/Login.tsx
@@ -0,0 +1,10 @@
+import { IonContent } from "@ionic/react";
+
+const Login: React.FC = () => {
+  return (
+    <IonContent className="ion-padding">
+      <h1 className="text-3xl font-bold underline text-red">Hello world!</h1>
+    </IonContent>
+  );
+};
+export default Login;
