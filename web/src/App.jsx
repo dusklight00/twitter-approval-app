b69fce05d7ca6be2e60b107337c0961396ac34c3
dusklight00 <r.rahul.developer@gmail.com>
2024-03-30 23:40:10 +0530

    user-card component complete

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,33 @@
+import {
+  Container,
+  IconButton,
+  Button,
+  Typography,
+  Stack,
+} from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
+import UserCard from "./components/UserCard";
+
 function App() {
-  return <>asd</>;
+  return (
+    <>
+      <Container>
+        <Stack direction="row" justifyContent="space-between" className="pt-10">
+          <Button variant="contained" startIcon={<AddIcon />}>
+            Create
+          </Button>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </Stack>
+        <Typography variant="h5" component="h5" my="30px">
+          <b>Users</b>
+        </Typography>
+        <UserCard />
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const darkTheme = createTheme({
   palette: {
