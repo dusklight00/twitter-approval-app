de5c4dd00c2b8337be1311b7b287aca009199e6a
dusklight00 <r.rahul.developer@gmail.com>
2024-03-30 23:47:54 +0530

    system-dashboard page complete

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,36 +1,9 @@
-import {
-  Container,
-  IconButton,
-  Button,
-  Typography,
-  Stack,
-} from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
-import AddIcon from "@mui/icons-material/Add";
-
-import AdminCard from "./components/AdminCard";
-import UserCard from "./components/UserCard";
+import SystemDashboard from "./pages/SystemDashboard";
 
 function App() {
   return (
     <>
-      <Container>
-        <Stack direction="row" justifyContent="space-between" className="pt-10">
-          <Button variant="contained" startIcon={<AddIcon />}>
-            Create
-          </Button>
-          <IconButton>
-            <SettingsIcon />
-          </IconButton>
-        </Stack>
-        <Typography variant="h5" component="h5" my="30px">
-          <b>Users</b>
-        </Typography>
-        <Stack gap={3}>
-          <AdminCard />
-          <UserCard />
-        </Stack>
-      </Container>
+      <SystemDashboard />
     </>
   );
 }
