f3db9077290e32597bb76980ebc4073be6e141c0
Blink29 <purukumar2905@gmail.com>
2024-04-12 16:09:20 +0530

    updated axios instance

--- web/src/instance.jsx
+++ web/src/instance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://twitter-approval-app-production-28af.up.railway.app/",
 });
 
 export default instance;
